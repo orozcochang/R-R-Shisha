0e42bbf9c8e30775b8b4040e7ff89ab3850cd785
orozcochang <orozco_chang@hotmail.com>
2023-01-23 03:07:19 +0900

    Set title icon

---
+++ frontend/src/actions/auth.jsx
@@ -0,0 +1,14 @@
+import {
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
+} from '../actions/types'
+
+export const load_user = () => async dispatch => {
+
+}
+
+export const login = (email, password) => async dispatch => {
+    
+}
