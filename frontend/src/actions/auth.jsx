a90368ad79cbf6f688bad8d38e0525e777d96120
orozcochang <orozco_chang@hotmail.com>
2023-02-05 07:08:46 +0900

    Worked on login

--- frontend/src/actions/auth.jsx
+++ frontend/src/actions/auth.jsx
@@ -7,47 +7,48 @@ import {
 } from './types'
 
 export const load_user = () => async dispatch => {
- if ( localStorage.getItem('access')){
-    const config = {
-        headers : {
-            'Content-Type': 'application/json',
-            'Authorization': `JWT ${localStorage.getItem('access')}`,
-            'Accept':'application/json'
-        }
-    }
-    try{
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/users/me/`, config)
-        dispatch({
-            type: LOAD_USER_SUCCESS,
-            payload: res.data
-        })
-            } catch(err){
-                dispatch({
-                    type: LOAD_USER_FAIL
-                })
+    if (localStorage.getItem('access')) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                'Accept': 'application/json'
             }
- } else {
-    dispatch({
-        type: LOAD_USER_FAIL
-    })
- }
+        }
+        try {
+            const res = await axios.get(`http://127.0.0.1:8000/auth/users/me/`, config)
+            dispatch({
+                type: LOAD_USER_SUCCESS,
+                payload: res.data
+            })
+        } catch (err) {
+            dispatch({
+                type: LOAD_USER_FAIL
+            })
+        }
+    } else {
+        dispatch({
+            type: LOAD_USER_FAIL
+        })
+    }
 }
 
 export const login = (email, password) => async dispatch => {
     const config = {
-        headers : {
+        headers: {
             'Content-Type': 'application/json'
         }
     }
-    const body = JSON.stringify({email,password})
+    const body = JSON.stringify({ email, password })
 
-    try{
-const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/create/`, body , config)
-dispatch({
-    type: LOGIN_SUCCESS,
-    payload: res.data
-})
-    } catch(err){
+    try {
+        const res = await axios.post(`http://127.0.0.1:8000/auth/jwt/create/`, body, config)
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+        dispatch(load_user())
+    } catch (err) {
         dispatch({
             type: LOGIN_FAIL
         })
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 function Home() {
-
     return (
         <div>
             <h1>Home</h1>
