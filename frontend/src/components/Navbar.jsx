69fc06be5aac233cc911b1784de7d59935da6dc2
orozcochang <orozco_chang@hotmail.com>
2023-02-05 22:22:17 +0900

    Created navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-function Navbar(){
+function Navbar() {
 
-    return(
-        <div>
-<h4><Link to=''>Navbar</Link></h4>
-        </div>
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="">R&R シーシャ</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/signup">Sign up</Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -3,9 +3,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/login">Login</Link>
+        <div className="container">
+            <div class="p-5 mb-4 bg-light rounded-3 mt-5">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">Welcome to R&R シーシャ!</h1>
+                    <p class="col-md-8 fs-4">Click the log in button to continue.</p>
+                    <Link to="/login"><button class="btn btn-primary btn-lg" type="button">Login</button> </Link>
+                </div>
+            </div>
         </div>
     )
 }
